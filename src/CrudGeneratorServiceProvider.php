b05e2b937b7542501e2fa9b2a6d1d983f400c57e
Matheus-note <matheuscarvalho.mcdc@gmail.com>
2019-06-18 19:19:07 -0300

    Initial commit

---
+++ src/CrudGeneratorServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace matheuscarvalho\crudgenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrudGeneratorServiceProvider extends ServiceProvider {
+    public function boot()
+    {
+
+    }
+    public function register()
+    {
+
+    }
+}
